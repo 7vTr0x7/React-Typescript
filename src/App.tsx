df7fe0c755b185929cc5ad569970905eaaff9731
7vTr0x7 <7vtrox7@gmail.com>
2024-03-16 14:40:32 +0530

    react-ts components func as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Box from "./components/Box";
 
 function App() {
+  const addFunc = (n: number, m: number): number => {
+    return n + m;
+  };
+
   return (
     <>
       <div>
-        <Box heading={"Hamlo"} count={1} />
+        <Box heading={"Hamlo"} count={1} addFunc={addFunc} />
       </div>
     </>
   );
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,13 +1,16 @@
 type PropType = {
   heading: string;
   count: number;
+  addFunc: (n: number, m: number) => number;
 };
 
-const Box = ({ heading, count }: PropType) => {
+const Box = ({ heading, count, addFunc }: PropType) => {
+  const value = addFunc(2, 5);
   return (
     <div>
       <h1>{heading}</h1>
       <p>{count}</p>
+      <p>{value}</p>
     </div>
   );
 };
