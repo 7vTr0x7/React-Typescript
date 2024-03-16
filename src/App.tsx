fb48b6e344bea57bc589230e19d4c50eef4c69e7
7vTr0x7 <7vtrox7@gmail.com>
2024-03-16 15:05:22 +0530

    react-ts Generic functions

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,18 @@ function App() {
     return n + m;
   };
 
+  const returnValue = <T,>(val: T): T => {
+    return val;
+  };
+
   return (
     <>
       <div>
-        <Box heading={"Hamlo"} count={1} addFunc={addFunc}>
+        <Box
+          heading={"Hamlo"}
+          count={1}
+          addFunc={addFunc}
+          returnValue={returnValue}>
           <h2>children</h2>
         </Box>
       </div>
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -5,10 +5,13 @@ type PropType = {
   count: number;
   addFunc: (n: number, m: number) => number;
   children: ReactNode;
+  returnValue: <T>(val: T) => T;
 };
 
-const Box = ({ heading, count, addFunc, children }: PropType) => {
+const Box = ({ heading, count, addFunc, children, returnValue }: PropType) => {
   const value = addFunc(2, 5);
+  const rValue1 = returnValue(1);
+  const rValue2 = returnValue("value");
 
   return (
     <div>
@@ -16,6 +19,8 @@ const Box = ({ heading, count, addFunc, children }: PropType) => {
       <p>{count}</p>
       <p>{value}</p>
       {children}
+      <p>{rValue1}</p>
+      <p>{rValue2}</p>
     </div>
   );
 };
