81718e1e005e2eeb100f0de8c6f83aec5a697362
7vTr0x7 <7vtrox7@gmail.com>
2024-03-17 19:22:29 +0530

    react-ts ts in useReducer Hook

--- src/components/UseReducer.tsx
+++ src/components/UseReducer.tsx
@@ -1,4 +1,18 @@
+import { useReducer } from "react";
+
+type StateType = {
+  count: number;
+};
+
+const initialState: StateType = {
+  count: 0,
+};
+
+const reducer = () => {};
+
 const UseReducer = () => {
+  const {} = useReducer(reducer, initialState);
+
   return <div>UseReducer</div>;
 };
 
